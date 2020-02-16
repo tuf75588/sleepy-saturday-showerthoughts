095d5eee75c9beca95d68da5e2073ddfc2a64a80
Andrew Davis <atd285@gmail.com>
2020-02-15 20:04:27 -0500

    working!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import VueFullPage from "vue-fullpage.js";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(VueFullPage);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
